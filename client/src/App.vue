bf1ebe4848fa6dd77ab26a9df3b979a31ab8a005
Hoàng Trung <trung.nguyencasau@gmail.com>
2021-10-17 02:23:07 +0700

    + Add sass-loader package + Add theme footer + Add global font open sans

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,23 +2,26 @@
   <div>
     <div>Hello there</div>
     <theme-button msg="Button"/>
+    <theme-footer/>
   </div>
 </template>
 
 <script>
 import PictureFrame from '@/components/PictureFrame.vue'
 import ThemeButton from '@/components/ThemeButton.vue'
+import ThemeFooter from './components/ThemeFooter.vue'
 export default {
   components: {
     PictureFrame,
-    ThemeButton
+    ThemeButton,
+    ThemeFooter,
   }
 }
 </script>
 
-<style>
+<style src="./styles/styles.css">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: OpenSans;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ client/src/constants/constants.js
@@ -0,0 +1,5 @@
+const theme_brown = '#caa892';
+const theme_jade = '#37806b';
+const theme_black = '#111111';
+const theme_gray = '#646464';
+const theme_fore = '#eeeeee';
